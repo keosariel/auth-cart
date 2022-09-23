cb0be84c4fbeb39968c528e8ad93369a64a303c5
keosariel <keosariel@icloud.com>
2022-09-23 17:02:33 +0100

    fix: req

--- app/__init__.py
+++ app/__init__.py
@@ -14,11 +14,6 @@ api = Api(app)
 bcrypt = Bcrypt(app)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
 
-@app.after_request
-def add_header(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
-
 from app.users import *
 from app.cart import *
 from app.models import *
